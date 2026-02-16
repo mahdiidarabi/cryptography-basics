9962a211f4000f985e13f397a2ffba041ee1bf64
mahdiidarabi <mahdida97@gmail.com>
2026-02-16 15:03:39 +0330

    real example by adding hash of random values

--- zk2/get_hash.js
+++ zk2/get_hash.js
@@ -3,12 +3,17 @@
  * Compute Poseidon(secret) - the public hash for a given secret.
  * Use the same secret in input.json when proving.
  * Usage: node get_hash.js [secret]
+ *        node get_hash.js random   # use a random secret (Poseidon field element)
  * Default secret: 12345
  */
 const { buildPoseidon } = require("circomlibjs");
 
 async function main() {
-  const secret = process.argv[2] || "12345";
+  let secret = process.argv[2] || "12345";
+  if (secret === "random") {
+    const poseidon = await buildPoseidon();
+    secret = poseidon.F.toString(poseidon.F.random());
+  }
   const poseidon = await buildPoseidon();
   const hash = poseidon([BigInt(secret)]);
   const knownHash = poseidon.F.toString(hash);
